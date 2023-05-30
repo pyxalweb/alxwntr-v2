fc157b7f7cd88afa68ec6693165b1b3fd721236b
Alex Winter <alex@pyxalweb.com>
2023-05-29 23:22:52 -0700

    Header navigation

--- template-parts/site-navigation.php
+++ template-parts/site-navigation.php
@@ -1,10 +1,12 @@
-<?php
-wp_nav_menu( array(
-    'theme_location' => 'menu-1', // Replace with the location of your menu
-    'container' => 'nav', // Remove the <div> wrapper
-    'menu_class' => '', // Add a class to the parent <ul> wrapper
-    'list_item_class' => 'nav-item', // Add a class to all <li> elements
-    'link_class' => 'nav-link' // Add a class to all <a> elements
-    // TODO: Figure out how to add a class to the second level (dropdown) <ul> element
-));
-?>
+<div class="header__navigation">
+    <?php
+    wp_nav_menu( array(
+        'theme_location' => 'menu-1', // Replace with the location of your menu
+        'container' => 'nav', // Remove the <div> wrapper
+        'menu_class' => '', // Add a class to the parent <ul> wrapper
+        'list_item_class' => 'nav-item', // Add a class to all <li> elements
+        'link_class' => 'nav-link' // Add a class to all <a> elements
+        // TODO: Figure out how to add a class to the second level (dropdown) <ul> element
+    ));
+    ?>
+</div>
