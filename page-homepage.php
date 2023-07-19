53fbbd55b853c006d538ae89bc815e82d59ec1cf
Alex Winter <alex@pyxalweb.com>
2023-07-18 20:33:47 -0700

    Created get_posts_by_year function

--- page-homepage.php
+++ page-homepage.php
@@ -5,29 +5,41 @@ get_header();
 ?>
 
 <main id="site-main" class="site-main">
-    <section class="content width-df | mbl-7 mbl-3-vw400">
-        <?php
-        $posts2023 = new WP_Query(array (
+    <?php
+    function get_posts_by_year($year) {
+        $posts = new WP_Query(array (
             'post_type' => 'post',
             'ignore_sticky_posts' => 1,
-            'year' => '2023',
-            'cat' => '-4, -7'
+            'year' => $year,
+            'cat' => '-1, -4, -7'
         ));
 
-        while($posts2023->have_posts()) {
-            $posts2023->the_post();
-        ?>
+        while($posts->have_posts()) {
+            $posts->the_post();
+            ?>
 
-        <div class="post-item">
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <div class="post-meta">
-                <p><?php the_time('m/d'); ?> - <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?></p>
+            <div class="post-item">
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <div class="post-meta">
+                    <p><?php the_time('m/d'); ?> - <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?></p>
+                </div>
             </div>
-        </div>
 
-        <?php
+            <?php
         }
-        ?>
+    }
+    ?>
+
+    <section class="content width-df | mbl-7 mbl-3-vw400">
+        <?php get_posts_by_year('2023'); ?>
+    </section>
+
+    <section class="content width-df | mbl-7 mbl-3-vw400">
+        <?php get_posts_by_year('2022'); ?>
+    </section>
+
+    <section class="content width-df | mbl-7 mbl-3-vw400">
+        <?php get_posts_by_year('2021'); ?>
     </section>
 </main>
 
--- page-test-page.php
+++ page-test-page.php
@@ -4,9 +4,9 @@
 get_header();
 ?>
 
-<main id="site-content" <?php post_class($class = 'site-content interior'); ?>>
+<main id="site-main" <?php post_class($class = 'site-content interior'); ?>>
     <section class="content width-df | mbl-7 mbl-3-vw400">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); the_content(); endwhile; endif; ?>
     </section>
 </main>
 
