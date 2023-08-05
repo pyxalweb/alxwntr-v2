ab0d72e03b244e2a5468687af731d1b372868cbd
Alex Winter <alex@pyxalweb.com>
2023-08-04 23:12:29 -0700

    New homepage posts layout

--- page-homepage.php
+++ page-homepage.php
@@ -7,21 +7,25 @@ get_header();
 <main id="site-main" class="site-main">
     <?php
     function get_posts_by_year($year) {
+        // surpress categories by adding:
+        // 'cat' => '-1,-2,-3' (where 1, 2, 3 are the category IDs)
         $posts = new WP_Query(array (
             'post_type' => 'post',
             'ignore_sticky_posts' => 1,
-            'year' => $year,
-            'cat' => '-1, -4, -7'
+            'year' => $year
         ));
 
         while($posts->have_posts()) {
             $posts->the_post();
             ?>
 
-            <div class="post-item">
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <div class="post-meta">
-                    <p><?php the_time('m/d'); ?> - <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?></p>
+            <div class="post__item">
+                <h3 class="post__heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <div class="post__category">
+                    <p><?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?></p>
+                </div>
+                <div class="post__date">
+                    <p><?php the_time('F jS'); ?></p>
                 </div>
             </div>
 
@@ -32,17 +36,23 @@ get_header();
 
     <section class="posts | content width-df">
         <h2>2023</h2>
-        <?php get_posts_by_year('2023'); ?>
+        <div class="post__container">
+            <?php get_posts_by_year('2023'); ?>
+        </div>
     </section>
 
     <section class="posts | content width-df">
         <h2>2022</h2>
-        <?php get_posts_by_year('2022'); ?>
+        <div class="post__container">
+            <?php get_posts_by_year('2022'); ?>
+        </div>
     </section>
 
     <section class="posts | content width-df">
         <h2>2021</h2>
-        <?php get_posts_by_year('2021'); ?>
+        <div class="post__container">
+            <?php get_posts_by_year('2021'); ?>
+        </div>
     </section>
 </main>
 
