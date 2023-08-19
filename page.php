fc17e54a5e38f4fbcc9ea7b60d3fd03b7b7a7865
Alex Winter <alex@pyxalweb.com>
2023-08-19 00:37:24 -0700

    the_title and the_post

--- page.php
+++ page.php
@@ -5,14 +5,18 @@ get_header();
 <?php
 while ( have_posts() ) :
 	the_post();
-endwhile;
 ?>
 
 <main id="site-content" <?php post_class($class = 'site-content interior'); ?>>
 	<section class="content width-df | mbl-7 mbl-3-vw400">
-        <p>main content</p>
+        <h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
     </section>
 </main>
+
+<?php
+endwhile;
+?>
 
 <?php
 get_footer();
