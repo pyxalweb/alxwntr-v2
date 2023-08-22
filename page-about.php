ff02640f5674622295f2d93769d813fa86bb9b0e
Alex Winter <alex@pyxalweb.com>
2023-08-22 00:32:21 -0700

    textphoto class
    Might rename this class later

--- page-about.php
+++ page-about.php
@@ -11,26 +11,33 @@ while ( have_posts() ) :
 
 <main id="site-main" <?php post_class($class = 'site-main interior'); ?>>
 	<section class="content width-df | text-100 | mbl-3">
-        <h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-
-        <div class="slideshow" data-interval="4000">
-            <div class="slideshow__slide" data-status="active">
-                <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-egyptian-coffee@original.jpg" alt="Alex Winter drinking Egyptian coffee">
-            </div>
-            <div class="slideshow__slide">
-                <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-ipa-beer@original.jpg" alt="Alex Winter drinking an IPA">
-            </div>
-            <div class="slideshow__slide">
-                <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-old-coffee-mug@original.jpg" alt="Alex Winter drinking coffee from a very old mug">
+        <div class="textphoto">
+            <div class="textphoto__text">
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
             </div>
 
-            <div class="slideshow__controls">
-                <button class="slideshow__prev">Prev</button>
-                <button class="slideshow__next">Next</button>
-            </div>
+            <div class="textphoto__photo">
+                <div class="slideshow" data-interval="4000">
+                    <div class="slideshow__slide" data-status="active">
+                        <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-egyptian-coffee@original.jpg" alt="Alex Winter drinking Egyptian coffee">
+                    </div>
+                    <div class="slideshow__slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-ipa-beer@original.jpg" alt="Alex Winter drinking an IPA">
+                    </div>
+                    <div class="slideshow__slide">
+                        <img src="<?php echo get_template_directory_uri(); ?>/temp/alex-winter-old-coffee-mug@original.jpg" alt="Alex Winter drinking coffee from a very old mug">
+                    </div>
 
-            <div class="slideshow__dots"></div>
+                    <div class="slideshow__controls">
+                        <button class="slideshow__prev">Prev</button>
+                        <button class="slideshow__next">Next</button>
+                    </div>
+
+                    <div class="slideshow__dots"></div>
+                </div>
+            </div>
+        </div>
     </section>
 </main>
 
