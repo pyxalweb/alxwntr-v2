73c37ca584e95182a8f781c400e11f26a4dfcca2
Alex Winter <alex@pyxalweb.com>
2023-08-29 21:18:19 -0700

    Fixed "skip to main content" link

--- template-parts/site-skip-nav.php
+++ template-parts/site-skip-nav.php
@@ -1,4 +1,4 @@
 <div id="skip-nav" role="complementary" aria-label="Skip Navigation">
-	<a href="#site-content">Skip to main content</a>
+	<a href="#site-main">Skip to main content</a>
 	<a href="#site-footer">Skip to footer</a>
 </div>
