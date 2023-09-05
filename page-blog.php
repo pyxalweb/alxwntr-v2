59ca26c2a0eaa9cd8ddff2c925fe52efd8b5c756
Alex Winter <alex@pyxalweb.com>
2023-09-04 18:18:23 -0700

    Refactored showHidePostItems to support buttons (large viewports) and select options (small viewports)

---
+++ page-blog.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: Blog */ ?>
+
+<?php
+get_header();
+?>
+
+<main id="site-main" class="site-main homepage">
+    <section class="content width-df | blog" role="region" aria-label="Blog Posts">
+        <h1>Blog</h1>
+        <h2>2022</h2>
+
+        
+    </section>
+</main>
+
+<?php
+get_footer();
