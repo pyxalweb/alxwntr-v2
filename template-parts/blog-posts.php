54805c91ee785da13639ebff6c343bb4e2f79aac
Alex Winter <alex@pyxalweb.com>
2023-09-10 00:04:46 -0700

    Clean up + added ability to have different markup for the_loop on page-blog.php  and page-homepage.php

--- template-parts/blog-posts.php
+++ template-parts/blog-posts.php
@@ -11,33 +11,28 @@ $posts = new WP_Query(array (
     'posts_per_page' => -1,
     'date_query' => $date_query,
 ));
-?>
 
-<?php 
-if ($posts->have_posts()) {
-    if ($args['past_year']) {
-    ?>
-        <div class="blog__posts" role="group" aria-label="Articles from the past year">
-    <?php
-    }
-    else {
-    ?>
-        <div class="blog__posts" role="group" aria-label="Articles from <?php echo $year; ?>">
-        <h2><?php echo $year; ?></h2>
-    <?php
-    }
-    ?>
-        <div class="blog__posts__container">
-            <?php
-            // Loop through the posts
-            while($posts->have_posts()) {
-                $posts->the_post();
+if (!function_exists('the_loop')) {
+    function the_loop($posts, $pastYear) {
+        // Loop through the posts
+        while($posts->have_posts()) {
+            $posts->the_post();
 
-                foreach((get_the_category()) as $category) {
-                    $category_id = $category->cat_ID;
-                    $category_name = $category->cat_name; ?>
-
-                    
+            foreach((get_the_category()) as $category) {
+                $category_id = $category->cat_ID;
+                $category_name = $category->cat_name;
+                
+                if ($pastYear===true) { ?>
+                    <div class="post__item | active fade" data-category="<?php echo $category_id; ?>">
+                        <h3 class="post__heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <div class="post__category" aria-label="Category">
+                            <p><?php echo $category_name; ?></p>
+                        </div>
+                        <div class="post__date" aria-label="Date">
+                            <time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('F jS, Y'); ?></time>
+                        </div>
+                    </div>
+                <?php } else { ?>
                     <div class="post__item | active fade" data-category="<?php echo $category_id; ?>">
                         <h3 class="post__heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                         <div class="post__category" aria-label="Category">
@@ -49,12 +44,27 @@ if ($posts->have_posts()) {
                     </div>
                 <?php }
             }
-            ?>
+        }
+    }
+};
+
+if ($posts->have_posts()) {
+    if ($args['past_year']) { ?>
+        <div class="blog__posts" role="group" aria-label="Articles from the past year">
+            <div class="blog__posts__container">
+                <?php the_loop($posts, true);
+    }
+    else { ?>
+        <div class="blog__posts" role="group" aria-label="Articles from <?php echo $year; ?>">
+            <h2><?php echo $year; ?></h2>
+            <div class="blog__posts__container">
+                <?php the_loop($posts, false);
+    }
+    ?>
+            </div>
         </div>
-    </div>
- <?php
-} else {
-        // there are no posts for this year
+<?php } else {
+        // there are no posts
         return;
     }
 ?>
