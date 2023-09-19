442ed1df40f9a008b75d496f0db04b5042cd19fd
Alex Winter <alex@pyxalweb.com>
2023-09-18 23:33:12 -0700

    Added favicons

--- header.php
+++ header.php
@@ -15,6 +15,15 @@
 
 	<title><?php wp_title('-', 'echo', 'right'); ?></title>
 	<?php wp_head(); ?>
+
+	<!-- icons -->
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon-dark-mode.png" media="(prefers-color-scheme: dark)">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon-light-mode.png" media="(prefers-color-scheme: light)">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/apple-touch-icon.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/logo-schema.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="680">
 </head>
 
 <body <?php body_class('preload'); ?>>
