e8fa7446e904cc075e658be24c159debd76c3c6e
Alex Winter <alex@pyxalweb.com>
2023-09-20 21:57:27 -0700

    Icons moved to template-parts

--- header.php
+++ header.php
@@ -16,14 +16,7 @@
 	<title><?php wp_title('-', 'echo', 'right'); ?></title>
 	<?php wp_head(); ?>
 
-	<!-- icons -->
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon-dark-mode.png" media="(prefers-color-scheme: dark)">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon-light-mode.png" media="(prefers-color-scheme: light)">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/apple-touch-icon.png">
-    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/logo-schema.png">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="680">
+	<?php get_template_part( 'template-parts/head', 'icons' ); ?>
 
     <?php get_template_part( 'template-parts/head', 'analytics' ); ?>
 </head>
