130e3faf38e39e46e4b7538fa80713f1731c609f
Alex Winter <alex@pyxalweb.com>
2023-09-20 21:54:12 -0700

    Indent

--- template-parts/head-analytics.php
+++ template-parts/head-analytics.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GMLJF7GTXY"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GMLJF7GTXY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'G-GMLJF7GTXY');
-</script>
+        gtag('config', 'G-GMLJF7GTXY');
+    </script>
