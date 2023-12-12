6ccfd74e7498ccd7b2a14031831e61faf6305e2e
Alex Winter <alex@pyxalweb.com>
2023-12-11 22:02:45 -0800

    Initial commit for "Block: Highlighted Image"

---
+++ blocks/highlighted-image/highlighted-image.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Highlighted Image Block template.
+ *
+ * @param array $block The block settings and attributes.
+ */
+
+// Load values and assign defaults.
+$image             = get_field( 'image' );
+$caption           = !empty(get_field( 'caption' )) ? get_field( 'caption' ) : 'Your text here...';
+$arrange           = get_field( 'arrange' ); // ACF's radio button.
+
+// Create class attribute allowing for custom "className" and "align" values.
+$class_name = 'highlighted-image';
+if ( ! empty( $block['className'] ) ) {
+    $class_name .= ' ' . $block['className'];
+}
+if ( ! empty( $block['align'] ) ) {
+    $class_name .= ' align' . $block['align'];
+}
+?>
+
+<figure class="<?php echo esc_attr( $class_name ); ?>">
+    <div class="highlighted-image__image">
+        <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'highlighted-image__image__img' ) ); ?>
+    </div>
+
+    <figcaption class="highlighted-image__caption">
+        <?php echo esc_html( $caption ); ?>
+    </figcaption>
+</figure>
+</div>
