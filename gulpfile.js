bda5990805869d402fe4208c4d32aa0244691ef3
Alex Winter <alex@pyxalweb.com>
2024-01-04 22:58:19 -0800

    Organized things and added Sounds block

--- gulpfile.js
+++ gulpfile.js
@@ -4,52 +4,19 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 
+
+
+
+// ***********************************
+//  Main Tasks
+// ***********************************
+
 // Sass Task
 function scssTask(){
   return src('app/scss/styles.scss', { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
     .pipe(dest('dist', { sourcemaps: '.' }));
-}
-
-// Sass Task: Testimonial Example 1
-function testimonialScssTask() {
-  return src('blocks/testimonial-example-1/testimonial.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('blocks/testimonial-example-1', { sourcemaps: '.' }));
-}
-
-// Sass Task: Zig Zag
-function zigZagScssTask() {
-  return src('blocks/zig-zag/zig-zag.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('blocks/zig-zag', { sourcemaps: '.' }));
-}
-
-// Sass Task: Highlighted Image
-function highlightedImageScssTask() {
-  return src('blocks/highlighted-image/highlighted-image.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('blocks/highlighted-image', { sourcemaps: '.' }));
-}
-
-// Sass Task: Image Slideshow
-function imageSlideshowScssTask() {
-  return src('blocks/image-slideshow/image-slideshow.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('blocks/image-slideshow', { sourcemaps: '.' }));
-}
-
-// Sass Task: Link Button
-function linkButtonScssTask() {
-  return src('blocks/link-button/link-button.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('blocks/link-button', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
@@ -68,29 +35,98 @@ function watchTask(){
   watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask));
 }
 
-// Watch Task: Testimonial Example 1
-watch('blocks/testimonial-example-1/testimonial.scss', testimonialScssTask);
 
+
+
+// ***********************************
+//  ACF Blocks
+// ***********************************
+
+// Sass Task: Zig Zag
+function zigZagScssTask() {
+  return src('blocks/zig-zag/zig-zag.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('blocks/zig-zag', { sourcemaps: '.' }));
+}
 // Watch Task: Zig Zag
-watch('blocks/zig-zag/zig-zag.scss', zigZagScssTask);
+watch(['blocks/zig-zag/zig-zag.scss'], zigZagScssTask);
 
+
+
+
+// Sass Task: Highlighted Image
+function highlightedImageScssTask() {
+  return src('blocks/highlighted-image/highlighted-image.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('blocks/highlighted-image', { sourcemaps: '.' }));
+}
 // Watch Task: Highlighted Image
-watch('blocks/highlighted-image/highlighted-image.scss', highlightedImageScssTask);
+watch(['blocks/highlighted-image/highlighted-image.scss'], highlightedImageScssTask);
 
-// Watch Task: Highlighted Image
-watch('blocks/highlighted-image/highlighted-image.scss', highlightedImageScssTask);
 
+
+
+// Sass Task: Image Slideshow
+function imageSlideshowScssTask() {
+  return src('blocks/image-slideshow/image-slideshow.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('blocks/image-slideshow', { sourcemaps: '.' }));
+}
+// Watch Task: Image Slideshow
+watch(['blocks/image-slideshow/image-slideshow.scss'], imageSlideshowScssTask);
+
+
+
+
+// Sass Task: Link Button
+function linkButtonScssTask() {
+  return src('blocks/link-button/link-button.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('blocks/link-button', { sourcemaps: '.' }));
+}
 // Watch Task: Link Button
-watch('blocks/image-slideshow/image-slideshow.scss', imageSlideshowScssTask);
+watch(['blocks/link-button/link-button.scss'], linkButtonScssTask);
+
+
+
+
+// Sass Task: Sounds
+function soundsScssTask() {
+  return src('blocks/sounds/sounds.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('blocks/sounds', { sourcemaps: '.' }));
+}
+// JavaScript Task: Sounds
+function soundsJsTask() {
+  return src('blocks/sounds/sounds.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('blocks/sounds', { sourcemaps: '.' }));
+}
+// Watch Task: Sounds
+watch('blocks/sounds/sounds.scss', soundsScssTask);
+watch('blocks/sounds/sounds.js', soundsJsTask);
+
+
+
+
+// ***********************************
+//  Exports
+// ***********************************
 
 // Default Gulp task
 exports.default = series(
   scssTask,
   jsTask,
   watchTask,
-  testimonialScssTask,
   zigZagScssTask,
   highlightedImageScssTask,
+  imageSlideshowScssTask,
   linkButtonScssTask,
-  imageSlideshowScssTask
+  soundsScssTask,
+  soundsJsTask
 );
