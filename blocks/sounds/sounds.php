fdce5864982f194a570bb7c3fc9356f01d2609c8
Alex Winter <alex@pyxalweb.com>
2024-01-08 23:39:57 -0800

    Sounds block which gets a repeater that contains a text input for the name of the sound and a URL input for the file path of the sound.

--- blocks/sounds/sounds.php
+++ blocks/sounds/sounds.php
@@ -1,37 +1,31 @@
-<div class="sounds">
-  <div class="sounds__item">
-    <div class="sounds__name">
-      <p>StartupMacQuadra.wav</p>
+<?php
+/**
+ * Sounds Block template.
+ *
+ * @param array $block The block settings and attributes.
+ */
+
+// Retrieve the repeater field with all images
+$sounds = get_field('sounds');
+
+if ($sounds) : ?>
+    <div class="sounds">
+        <?php foreach ($sounds as $sound) :
+            if (!$sound['sound_name'] | !$sound['sound_url']) continue; // If no sound, skip to next sound (if any)
+            $sound_name = $sound['sound_name'];
+            $sound_url = $sound['sound_url'];
+        ?>
+        <div class="sounds__item">
+            <div class="sounds__name">
+                <p><?php echo esc_html( $sound['sound_name'] ); ?></p>
+            </div>
+            <div class="sounds__btn">
+                <button><span>Play</span></button>
+                <audio>
+                    <source src="<?php echo esc_url( $sound_url ); ?>" type="audio/wav">
+                </audio>
+            </div>
+        </div>
+        <?php endforeach; ?>
     </div>
-    <div class="sounds__btn">
-      <button><span>Play</span></button>
-      <audio controls controlslist="noplaybackrate">
-        <source src="https://alxwntr.com/downloads/Mac-OS-Sounds/StartupMacQuadra.wav" type="audio/wav">
-      </audio>
-    </div>
-  </div>
-  
-  <div class="sounds__item">
-    <div class="sounds__name">
-      <p>StartupPowerMacPCI.wav</p>
-    </div>
-    <div class="sounds__btn">
-      <button><span>Play</span></button>
-      <audio controls controlslist="noplaybackrate">
-        <source src="https://alxwntr.com/downloads/Mac-OS-Sounds/StartupPowerMacPCI.wav" type="audio/wav">
-      </audio>
-    </div>
-  </div>
-  
-  <div class="sounds__item">
-    <div class="sounds__name">
-      <p>StartupIntelPreT2Mac.wav</p>
-    </div>
-    <div class="sounds__btn">
-      <button><span>Play</span></button>
-      <audio controls controlslist="noplaybackrate">
-        <source src="https://alxwntr.com/downloads/Mac-OS-Sounds/StartupIntelPreT2Mac.wav" type="audio/wav">
-      </audio>
-    </div>
-  </div>
-</div>
+<?php endif; ?>
