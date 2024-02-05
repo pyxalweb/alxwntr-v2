ab75082eb1db38b49be4b0c4c3f90f2a8e1a11a8
Alex Winter <alex@pyxalweb.com>
2024-02-04 19:30:05 -0800

    Added support for 'featured images' to function as the Open Graph image and default to the 'logo-schema' png file in the theme folder if not found. I'm not currently using any featured images on any pages or posts but may do so in the future.

--- template-parts/head-icons.php
+++ template-parts/head-icons.php
@@ -3,6 +3,13 @@
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon-light-mode.png" media="(prefers-color-scheme: light)">
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/apple-touch-icon.png">
     <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/logo-schema.png">
+    <?php
+    if (has_post_thumbnail()) {
+        $featured_image_url = get_the_post_thumbnail_url();
+    } else {
+        $featured_image_url = get_template_directory_uri() . '/assets/logo-schema.png';
+    }
+    ?>
+    <meta property="og:image" content="<?php echo esc_url($featured_image_url); ?>">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="680">
