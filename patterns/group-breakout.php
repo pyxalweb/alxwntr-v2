619396a2d71283c1de7e91f42c9571524c2e6146
Alex Winter <alex@pyxalweb.com>
2024-02-17 16:45:45 -0800

    "Group: Breakout" pattern within the theme rather than database

---
+++ patterns/group-breakout.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Group - Breakout
+ * Slug: group-breakout
+ * Description: A group container that breaks out of the parent container.
+ * Categories: alxwntr/group
+ * Keywords: group, breakout, container
+ * Viewport Width: 100vw
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+<!--
+wp:group {
+    "backgroundColor":"grey-200",
+    "className":"group--breakout",
+    "layout":{
+        "type":"constrained"
+    },
+    "metadata":{
+        "name":"Group - Breakout"
+    }
+}
+-->
+<div class="wp-block-group has-grey-200-background-color has-background group--breakout">
+    <!--
+    wp:group {
+        "className":"content width-400",
+        "layout":{
+            "type":"constrained"
+        },
+        "metadata":{
+            "name":"Content"
+        }
+    }
+    -->
+    <div class="wp-block-group content width-400">
+        <!-- wp:paragraph -->
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil veritatis rerum distinctio alias blanditiis quae corrupti? Magnam, iste. Velit, sunt distinctio. Dolorum, laborum saepe. Animi beatae quo consequatur repellendus ipsam?</p>
+        <!-- /wp:paragraph -->
+    </div><!-- /wp:group -->
+</div><!-- /wp:group -->
