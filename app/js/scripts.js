84248c3d8bd79f43ea118d8420f6b1d7959f429c
Alex Winter <alex@pyxalweb.com>
2024-05-01 22:44:36 -0700

    Move 'Theme Toggle' to scripts.js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -21,4 +21,74 @@ window.onload = (event) => {
 
 
 
+// ***********************************
+//  Theme Toggle
+//  by Alex Winter - 2024-05-01 - v1.0
+// ***********************************
+
+const themeToggle = () => {
+	// **************************
+	//  Set and Get Cookie
+	// **************************
+	function setCookie(name, value, days) {
+		let expires = "";
+		if (days) {
+			let date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toUTCString();
+		}
+		document.cookie = name + "=" + (value || "") + expires + "; path=/";
+	}
+	function getCookie(name) {
+		let nameEQ = name + "=";
+		let ca = document.cookie.split(';');
+		for (let i = 0; i < ca.length; i++) {
+			let c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		}
+		return null;
+	}
+
+	// **************************
+	//  Light Mode
+	// **************************
+	function enableLightMode() {
+		document.documentElement.className = 'light-mode';
+		setCookie('theme', 'light', 7);
+	}
+
+	// **************************
+	//  Dark Mode
+	// **************************
+	function enableDarkMode() {
+		document.documentElement.className = 'dark-mode';
+		setCookie('theme', 'dark', 7);
+	}
+
+	// **************************
+	//  Check cookie on page load
+	//  & set theme accordingly
+	// **************************
+	const savedTheme = getCookie('theme');
+	if (savedTheme === 'light') {
+		enableLightMode();
+	} else {
+		enableDarkMode();
+	}
+
+	// **************************
+	//  Listen for button clicks
+	// **************************
+	const themeToggleLightMode = document.querySelector('.theme-toggle--light-mode');
+	const themeToggleDarkMode = document.querySelector('.theme-toggle--dark-mode');
+	if (!themeToggleLightMode || !themeToggleDarkMode) return;
+	themeToggleLightMode.addEventListener('click', enableLightMode);
+	themeToggleDarkMode.addEventListener('click', enableDarkMode);
+}
+themeToggle();
+
+
+
+
 })() // end IIFE
