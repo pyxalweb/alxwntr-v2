248cb647d04a2a3ccfb38f26164e81fb8212285d
Alex Winter <alex@pyxalweb.com>
2024-05-03 18:07:03 -0700

    If the URL is anything other than '.local' then run Google Analytics

--- template-parts/scripts-analytics.php
+++ template-parts/scripts-analytics.php
@@ -1,9 +1,14 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GMLJF7GTXY"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-GMLJF7GTXY');
-</script>
+<?php
+    // if the URL ends in '.local', don't load Google Analytics
+    if (strpos($_SERVER['HTTP_HOST'], '.local') === false) {
+?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GMLJF7GTXY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-GMLJF7GTXY');
+    </script>
+<?php
+    };
+ ?>
