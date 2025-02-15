6db0a929102ccdd0235961041f2a2165c4dc2d5d
Alex Winter <alex@pyxalweb.com>
2025-02-15 12:52:14 -0800

    Fix social media links and add support for screen readers

--- template-parts/content-social.php
+++ template-parts/content-social.php
@@ -1,17 +1,17 @@
 <ul class="social">
     <li class="social__instagram">
-        <a href="https://www.instagram.com/alx.wntr/" target="_blank" rel="noopener nofollow" aria-label="Instagram"></a>
+        <a href="https://www.instagram.com/alexbwinter/" target="_blank" rel="noopener" aria-label="Instagram"><span class="screen-reader-only">Instagram</span></a>
     </li>
     <li class="social__linkedin">
-        <a href="https://www.linkedin.com/in/alex-winter-38696626/" target="_blank" rel="noopener nofollow" aria-label="LinkedIn"></a>
+        <a href="https://www.linkedin.com/in/alex-winter-38696626/" target="_blank" rel="noopener" aria-label="LinkedIn"><span class="screen-reader-only">Linked In</span></a>
     </li>
     <li class="social__lastfm">
-        <a href="https://www.last.fm/user/aydoubleyou" target="_blank" rel="noopener" aria-label="Last.FM"></a>
+        <a href="https://www.last.fm/user/aydoubleyou" target="_blank" rel="noopener" aria-label="Last.FM"><span class="screen-reader-only">Last FM</span></a>
     </li>
     <li class="social__github">
-        <a href="https://github.com/pyxalweb" target="_blank" rel="noopener nofollow me" aria-label="GitHub"></a>
+        <a href="https://github.com/pyxalweb" target="_blank" rel="noopener" aria-label="GitHub"><span class="screen-reader-only">Git Hub</span></a>
     </li>
     <li class="social__codepen">
-        <a href="https://codepen.io/pyxal" target="_blank" rel="noopener" aria-label="CodePen"></a>
+        <a href="https://codepen.io/pyxal" target="_blank" rel="noopener" aria-label="CodePen"><span class="screen-reader-only">Code Pen</span></a>
     </li>
 </ul>
